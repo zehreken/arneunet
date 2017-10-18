ce5bdf4100764ecd8d9e56ec96b4f22be7d52e9a
Guchan Alkan <guchan@gram.gs>
2017-10-18 22:37:17 +0200

    Add initial files

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using arneunet.Source;
+
+namespace arneunet
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			int rowCount = 10;
+			int columnCount = 10;
+			var matrix = new float[rowCount, columnCount];
+			matrix[1, 0] = 1;
+			
+			Console.WriteLine(matrix.ToPrintable());
+		}
+	}
+}
---
+++ Source/MatrixUtility.cs
@@ -0,0 +1,36 @@
+﻿namespace arneunet.Source
+{
+	public static class MatrixUtility
+	{
+		public static T[,] Transpose<T>(T[,] matrix)
+		{
+			var row = matrix.GetLength(0);
+			var column = matrix.GetLength(1);
+			var transposed = new T[column, row];
+			for (int i = 0; i < row; i++)
+			{
+				for (int j = 0; j < column; j++)
+				{
+					transposed[j, i] = matrix[i, j];
+				}
+			}
+
+			return transposed;
+		}
+
+		public static string ToPrintable<T>(this T[,] matrix)
+		{
+			var s = "printable\n";
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					s += matrix[i, j] + ", ";
+				}
+				s += "\n";
+			}
+
+			return s;
+		}
+	}
+}
